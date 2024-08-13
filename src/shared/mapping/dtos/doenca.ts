36bd55055b83a87ac5920d7f646d1fd0a985ee18
VitorHugoCD <vitordelantonia@gmail.com>
2024-08-13 18:48:14 -0300

    feat: mapeamento da classe doenca

---
+++ src/shared/mapping/dtos/doenca.ts
@@ -0,0 +1,7 @@
+export type DoencaDTO = {
+    id: string,
+    nome: string,
+    codigo: string,
+    descricao: string, 
+    tipo: string,
+}
---
+++ src/shared/mapping/models/doenca.ts
@@ -0,0 +1,18 @@
+import { DoencaDTO } from "../dtos/doenca";
+
+export class Doenca {
+
+    id: string;
+    nome: string;
+    codigo: string;
+    descricao: string;
+    tipo: string;
+    
+    constructor(doencaDto: DoencaDTO) {
+        this.id = doencaDto.id;
+        this.nome = doencaDto.nome;
+        this.codigo = doencaDto.codigo;
+        this.descricao = doencaDto.descricao;
+        this.tipo = doencaDto.tipo;
+    }
+}
