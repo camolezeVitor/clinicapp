bc71f5942e0cbc2c3e2e1bb3a0bc414126d02040
VitorHugoCD <vitordelantonia@gmail.com>
2024-08-14 19:48:37 -0300

    feat: indexação dos adapters

---
+++ src/shared/mapping/adapters/index.ts
@@ -0,0 +1,14 @@
+export { ClinicaAdapter } from "./clinica.adapter";
+export { ConfiguracoesDoUsuarioAdapter } from "./configs-usuario.adapter";
+export { ConsultaAdapter } from "./consulta.adapter";
+export { ContaCorrenteAdapter } from "./conta-corrente.adapter";
+export { ContaAdapter } from "./conta.adapter";
+export { DoencaAdapter } from "./doenca";
+export { EnderecoAdapter } from "./endereco.adapter";
+export { FuncionarioAdapter } from "./funcionario.adapter";
+export { PacienteAdapter } from "./paciente.adapter";
+export { PagamentoAdapter } from "./pagamento.adapter";
+export { PessoaAdapter } from "./pessoa.adapter";
+export { ProntuarioAdapter } from "./prontuario.adapter";
+export { ServicoAdapter } from "./servico.adapter";
+export { UsuarioAdapter } from "./usuario.adapter";
