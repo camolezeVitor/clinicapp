4331c284c479290bf1e37ef9e9605c9e5328fbf2
VitorHugoCD <vitordelantonia@gmail.com>
2024-08-22 15:05:30 -0300

    feat: security pathing

---
+++ src/shared/security/index.ts
@@ -0,0 +1,4 @@
+export { AdvancedEncryptionSystemService } from "./criptography/aes.service";
+export { AuthInterceptor } from "./interceptors/auth-interceptor.service";
+export { TokenInterceptor } from "./interceptors/token-interceptor.service";
+export { TokenService } from "./token/token.service";
