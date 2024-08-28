4e1b9847e23dc187892a3e02683a5f414a5f08a9
VitorHugoCD <vitordelantonia@gmail.com>
2024-08-28 11:17:34 -0300

    fix: dynamic instantiate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
+import { Dynamic } from '@decorators';
+import { ScreenState } from '@enums/states';
+import { windowChangeTest$ } from '@services';
 
+class ComponentA {};
+class ComponentB {};
+
+@Dynamic<ScreenState>({
+  parameter: windowChangeTest$,
+  componentTemplates: [
+    {component: ComponentA, condition: ScreenState.DESKTOP},
+    {component: ComponentB, condition: ScreenState.MOBILE},
+  ]
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/shared/decorators/dynamic/dynamic.dec.ts
+++ src/shared/decorators/dynamic/dynamic.dec.ts
@@ -3,7 +3,7 @@ import { DynamicHandler } from "./dynamic.handler";
 
 export function Dynamic<T>({componentTemplates, parameter}: DynamicType<T>) {
     
-    return function instantiate(constructor: Function) {
+    return function(constructor: Function) {
         constructor.prototype.dynamicHandler = new DynamicHandler<T>(
             parameter, 
             componentTemplates,
