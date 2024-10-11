76d242862ef510116456d5f3d7a7ece8a648b3f7
VitorHugoCD <vitordelantonia@gmail.com>
2024-10-11 11:54:40 -0300

    fix: url internas corrigidas

--- src/shared/utils/models/protocols/dynamic.protocol.ts
+++ src/shared/utils/models/protocols/dynamic.protocol.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone, Type } from "@angular/core";
-import { DynamicComponentObservationService } from "@services";
+import { DynamicComponentObservationService } from "@utils/services";
 import { DynamicHandlerProtocol } from "./dynamic-handler.protocol";
 import { Template } from "src/shared/utils/models/types";
 import { BehaviorSubject } from "rxjs";
